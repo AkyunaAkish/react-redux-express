c6bfb03acb1e53b3904ab66b1df2ee70a6fbc136
Akyuna Akish <akyuna.akish@zayo.com>
2017-02-02 22:39:12 -0700

    Removed count from router file because it was not being used

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 
 import Layout from './components/layout';
 import Add from './components/add';
-import Count from './components/count';
 
 export default (
   <Route path='/' component={Layout}>
